8bb7df2eb1869dacfd6b2a57e484bd4101da4c65
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-22 03:46:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,14 +8,13 @@ def join_nested_strings(src)
   while src.length > i do 
     ii = 0
     while src[i].length > ii do 
-      if src[i][ii] 
-    
+      if (src[i][ii].class == String)
+        new_string.concat(src[i][ii])
+      end
+      ii +=1
     end
+    i += 1
   end
-  
-  
-  
-  
-  
-  
+
 end
+
